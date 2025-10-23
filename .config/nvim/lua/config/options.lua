154674aadef507c985b88fa76c26bfaca115012e
LuckedCoronet <lc45@lc-studios.net>
2025-10-23 14:20:50 +0900

    feat(nvim): update options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,27 +1,34 @@
 vim.o.tabstop = 4
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 vim.o.updatetime = 250
 vim.o.timeoutlen = 400
 vim.o.splitright = true
 vim.o.splitbelow = true
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 vim.o.cursorline = false
 vim.o.scrolloff = 10
+vim.o.sidescrolloff = 10
 vim.o.confirm = true
+vim.o.wrap = false
+
+-- Code folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99 -- Prevent all folds from closing on file open
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+	vim.o.clipboard = "unnamedplus"
 end)
