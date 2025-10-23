1e7193f369900bc1f0c576234c6cb6660c974fc7
LuckedCoronet <lc45@lc-studios.net>
2025-10-23 14:39:48 +0900

    feat(nvim): update mini.nvim config

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -2,6 +2,17 @@ return {
 	"nvim-mini/mini.nvim",
 	version = "*",
 	config = function()
+		require("mini.ai").setup()
+		require("mini.bracketed").setup()
+		require("mini.pairs").setup()
 		require("mini.surround").setup()
+
+		require("mini.files").setup({
+			windows = {
+				preview = true,
+				width_preview = 30,
+			},
+		})
+		vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<cr>", { desc = "Open file explorer" })
 	end,
 }
